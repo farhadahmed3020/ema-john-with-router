015ddb7f3bd6f19660986cecce00efffefa58359
farhadahmed3020 <farhadahmed3020@gmail.com>
2020-09-09 23:13:03 +0600

    first commit

---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetail = () => {
+    return (
+        <div>
+            <h1> Product detail coming soooon</h1>
+        </div>
+    );
+};
+
+export default ProductDetail;

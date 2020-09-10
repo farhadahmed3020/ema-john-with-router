7733bc456aa66a0c5100c10faedd2fda5b34f992
farhadahmed3020 <farhadahmed3020@gmail.com>
2020-09-10 21:39:30 +0600

    add new

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useEffect } from 'react';
+import {useState} from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import fakeData from '../../fakeData';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
+
+     const[ cart, setCart ] =useState([]);
+    useEffect(()=>{
+        //cart 
+
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const cartProducts = productKeys.map(key => {
+        const product = fakeData.find (pd => pd.key === key);
+        product.quantity = savedCart[key];
+        return product;
+    
+
+    });
+    setCart (cartProducts);
+    },[])
     return (
         <div>
-        <h2> this the good product</h2>
+        <h2> Cart Items:{cart.length}</h2>
+        {
+            cart.map(pd => <ReviewItem 
+                key ={pd.key}
+                product={pd}></ReviewItem>)
+        }
+    
         </div>
     );
 };
